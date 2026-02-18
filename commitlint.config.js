43508af7307dd5e91e287af54f5c8fbc4cf87423
KELVERIANO <kevin.doeding@hotmail.com>
2026-02-18 09:38:04 +0100

    fix: commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,13 @@
 export default {
-  extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'refactor']],
-    'subject-case': [0],
+    'type-enum': [2, 'always', ['feat', 'fix', 'chore']],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'type-case': [0],
+    'header-pattern': [
+      2,
+      'always',
+      '^(feat|fix|chore)(!)?: [a-zA-Z0-9_-]+$'
+    ],
   },
 };
